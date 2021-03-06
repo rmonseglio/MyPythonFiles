a3d63d6c8ed67f5ce6f5fdd97a83ce992d51d92e
rmonseglio <61055708+rmonseglio@users.noreply.github.com>
2021-03-06 10:05:46 -0500

    Add files via upload

---
+++ Homework7.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Feb 21 16:45:51 2021
+
+@author: Coding.Troll(Richard Monseglio)
+"""
+
+DictBandInfo = {"SongTitle: ":"BloodFeather","Artist: ":"Highly Suspect",
+                "Album: ":"Mister Asylum","DateReleased: ":"2015","Genre: ":"Alternative/Indie Rock",
+                "SongLength: ":"3:54"}
+
+for i, j in DictBandInfo.items():
+    print(i,j)
+    
